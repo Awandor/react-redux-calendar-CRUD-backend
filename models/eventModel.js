9d9c4fb5ba7e767e78af5d80ef98a61f4e061af2
Dan Häggblom <dan.haggblom@accenture.com>
2021-01-19 13:44:01 +0100

    añadido serialización de eventModel

--- models/eventModel.js
+++ models/eventModel.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require( 'mongoose' );
 
-const userSchema = Schema( {
+const eventSchema = Schema( {
   title: {
     type: String,
     required: true
@@ -23,5 +23,19 @@ const userSchema = Schema( {
   }
 } );
 
+// Vamos a serializar el modelo para que _id aparezca como id y quitamos __v
 
-module.exports = model( 'EventModel', userSchema );
+eventSchema.method( 'toJSON', function() {
+
+  // Usamos una función normal para no perder el this
+
+  const { __v, _id, ...object } = this.toObject();
+
+  object.id = _id;
+
+  return object;
+
+} );
+
+
+module.exports = model( 'EventModel', eventSchema );
